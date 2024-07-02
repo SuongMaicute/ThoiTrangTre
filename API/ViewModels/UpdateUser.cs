75a862909e5fdd1954b9b9e4b341f57288697f07
suongmaicute <maidtsse170085@fpt.edu.vn>
2024-07-02 10:04:33 +0700

    user

---
+++ API/ViewModels/UpdateUser.cs
@@ -0,0 +1,13 @@
+﻿namespace API.ViewModels
+{
+    public class UpdateUser
+    {
+        public int? userid { get; set; }
+        public string UsersName { get; set; } = null!;
+
+
+        public string UsersPhone { get; set; } = null!;
+
+        public string UsersAddress { get; set; } = null!;
+    }
+}
